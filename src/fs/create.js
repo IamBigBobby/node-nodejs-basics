016d46802d5baaf1729276b57eb783256f142eff
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-05 18:36:50 +0400

    feat: add solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { writeFile } from 'fs';
+
+const content = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    writeFile('./src/fs/files/fresh.txt', content, (err) => {
+        if(err) {
+            console.error("FS operation failed");
+            return;
+        }
+        console.log("File created");
+    }) 
 };
 
 await create();
