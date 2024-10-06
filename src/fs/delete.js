60427276f3992e9a96f61cee62594297744bed8e
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-06 14:04:48 +0400

    feat: add solution delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { existsSync } from 'fs';
+import { rm } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    try{
+        const pathDeletedFile = './src/fs/files/fileToRemove.txt'
+        const existPathDeletedFile = existsSync(pathDeletedFile);
+
+        if(!existPathDeletedFile){
+            throw Error('FS operation failed')
+        }
+
+        await rm(pathDeletedFile);
+    } catch (error){
+        console.log(error)
+        return;
+    }
 };
 
 await remove();
