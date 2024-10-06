4ece3c1b6103da5c2972094e7d9db81c3af7787d
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-06 14:15:23 +0400

    feat: add solution for list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import { existsSync } from 'fs';
+import { readdir, readFile, mkdir, writeFile } from 'fs/promises';
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+    try{
+        const dirPath = './src/fs/files';
+
+        const exists = existsSync(dirPath);
+
+        if(!exists){
+            throw Error('FS operation failed')
+        }
+
+        const readedFiles = await readdir(dirPath);
+
+        for(const files of readedFiles){
+            console.log(files);
+        }
+    } catch (error) {
+        console.log(error);
+        return;
+    }
 };
 
 await list();
