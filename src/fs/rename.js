6c056f66e3b920516228e4f3f3603787ea05e21d
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-06 12:39:24 +0400

    feat: add solution for rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { existsSync } from 'fs';
+import { rename as fsRename } from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    try{
+        const oldPath = './src/fs/files/wrongFilename.txt'
+        const newPath = './src/fs/files/properFilename.md'
+
+        const existsOldPath = existsSync(oldPath);
+        const existsNewPath = existsSync(newPath);
+
+            if(existsNewPath || existsOldPath){
+                throw Error('FS operation failed')
+            }
+
+        await fsRename(oldPath, newPath)
+    } catch (error) {
+        console.log(error)
+        return;
+    }
 };
 
 await rename();
