b43351133b4efc4e09ebca627a7c7df3252120a6
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-06 12:18:59 +0400

    feat: add solution for copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,40 @@
+import { existsSync } from 'fs';
+import { readdir, readFile, mkdir, writeFile } from 'fs/promises';
+import path from "path";
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    
+    try {
+        const dirPath = './src/fs/files';
+        const copyPath = './src/fs/files_copy';
+
+        const exists = existsSync(copyPath);
+
+        if(exists){
+            throw Error('files_copy already exist')
+        }
+
+        mkdir(copyPath, {recursive: true}, (error) => {
+            if (error){
+                throw error;
+            }
+        })
+
+        const readedFiles = await readdir(dirPath);
+
+        for (const file of readedFiles){
+            const filePath = path.join(dirPath, file);
+            const fileContent = await readFile(filePath);
+
+            const newFilePath = path.join(copyPath, file);
+            await writeFile(newFilePath, fileContent);
+        }
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
 };
 
 await copy();
