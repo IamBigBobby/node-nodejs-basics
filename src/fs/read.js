9d7237d7c0b85575d45ef1493b0d0854f9cd990e
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-06 14:28:10 +0400

    feat: add solution for read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    try{
+        const dirPathToRead = './src/fs/files/fileToRead.txt';
+
+        const exists = existsSync(dirPathToRead);
+
+        if(!exists){
+            throw Error('FS operation failed')
+        }
+
+        const fileContent = await readFile(dirPathToRead, 'utf-8');
+
+        console.log(fileContent);
+    } catch (error) {
+        console.log(error);
+        return;
+    }
 };
 
 await read();
