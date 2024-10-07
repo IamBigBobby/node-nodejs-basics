4fb4ac8af11e1e0d7f01584b3433610e1dbdbae8
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-07 12:57:55 +0400

    feat: add solution for calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
+
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(filePath);
+
+    stream.on('data', (data) => {
+        hash.update(data);
+    });
+
+    stream.on('end', () => {
+        const hexHash = hash.digest('hex');
+        console.log(`SHA256 Hash of ${filePath}: ${hexHash}`);
+    });
+
+    stream.on('error', (err) => {
+        console.error(`Error reading file: ${err.message}`);
+    });
 };
 
 await calculateHash();
