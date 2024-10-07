82cf69fa1d94945250e05627acfe0108fdd434e7
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-07 14:28:51 +0400

    feat: add solution for main.js and worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCPUs = cpus().length;
+    const workers = [];
+    const results = [];
+
+    for (let i = 0; i < numCPUs; i += 1) {
+        const worker = new Worker("./src/wt/worker.js", { workerData: 10 + i });
+
+        worker.on("message", (data) => {
+            results.push({ status: "resolved", data });
+        });
+
+        worker.on("error", (err) => {
+            results.push({ status: "error", data: null });
+        });
+
+        workers.push(worker);
+    }
+
+    await Promise.allSettled(
+        workers.map((worker) => new Promise((result) => worker.on("exit", result)))
+      );
+      console.log(results);
 };
 
 await performCalculations();
