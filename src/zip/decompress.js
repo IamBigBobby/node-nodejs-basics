01ea3c6bfbe615150b13f83cd835bced0e4b9ea2
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-07 13:30:17 +0400

    feat: add solution for decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const fileToDeCompress = 'src/zip/files/archive.gz';
+    const outputFile = 'src/zip/files/fileToCompressAfterDecompress.txt';
+
+    const readStream = fs.createReadStream(fileToDeCompress);
+    
+    const writeStream = fs.createWriteStream(outputFile);
+
+    const gunzip = zlib.createGunzip();
+
+    readStream
+        .pipe(gunzip)
+        .pipe(writeStream)
+        .on('finish', () => {
+            console.log('File successfully decompressed to ' + outputFile);
+        })
+        .on('error', (err) => {
+            console.error('Error during decompression:', err);
+        });
 };
 
 await decompress();
