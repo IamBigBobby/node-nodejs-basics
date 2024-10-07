a6ae0d2809d57668ad01ab150d8f70564030a064
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-07 13:03:59 +0400

    feat: add solution for read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = 'src/streams/files/fileToRead.txt';
+    const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log('\nFinished reading the file.');
+    });
+
+    readStream.on('error', (err) => {
+        console.error('Error reading the file:', err);
+    });
 };
 
 await read();
